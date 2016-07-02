81a49f245fea2333a8d4740fba96981ee5e9ea24
Victor Mwenda <vmwenda.vm@gmail.com>
2016-07-02 12:03:06 +0300

    Method comments
    Included method comments

--- main/java/com/marvik/libs/android/security/authentication/AuthenticationUtils.java
+++ main/java/com/marvik/libs/android/security/authentication/AuthenticationUtils.java
@@ -7,6 +7,13 @@ import com.marvik.libs.android.utils.math.QuickMath;
  * Created by victor on 7/2/2016.
  */
 public class AuthenticationUtils {
+
+    /**
+     * Creates a hint of a password by replacing all the characters that are prime with a *
+     *
+     * @param password to hint
+     * @return hintedPassword
+     */
     public static String getPasswordHint(String password) {
         String passwordHint = "";
         int passwordLength = password.length();
