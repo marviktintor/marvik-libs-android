c329e422bff09026631171d4561d4b5af3827014
Victor Mwenda <vmwenda.vm@gmail.com>
2016-07-19 11:56:42 +0300

    User Accounts
    Added better methods to handle adding user accounts and checking if an
    account exists

--- main/java/com/marvik/libs/android/accounts/UserAccountsManager.java
+++ main/java/com/marvik/libs/android/accounts/UserAccountsManager.java
@@ -15,23 +15,85 @@ public class UserAccountsManager {
 
     }
 
+    /**
+     * Adds a sync account
+     *
+     * @param accountType account type
+     * @param accountName name of account
+     * @param authority   authority of the content provider
+     */
     public void addSyncAccount(String accountType, String accountName, String authority) {
-        if (!isAccountExists("account.type")) {
+        if (!isAccountExists(accountType)) {
             addAccount(accountType, accountName, authority);
         }
     }
 
-    private void addAccount(String accountType, String accountName, String authority) {
-        AccountManager accountManager = (AccountManager) getContext().getSystemService(Context.ACCOUNT_SERVICE);
+    /**
+     * Adds a sync account
+     *
+     * @param accountType account type
+     * @param accountName name of account
+     * @param authority   authority of the content provider
+     */
+    public void addAccount(String accountType, String accountName, String authority) {
+        addAccount(accountType, accountName, authority, false);
+    }
+
+    /**
+     * Adds a sync account
+     *
+     * @param accountType account type
+     * @param accountName name of account
+     * @param authority   authority of the content provider
+     * @param forceSync   force the account to sync
+     */
+    public void addAccount(String accountType, String accountName, String authority, boolean forceSync) {
 
         Account account = new Account(accountName, accountType);
 
-        accountManager.addAccountExplicitly(account, accountType, null);
+        addAccount(account);
 
-        forceSync(account, authority);
+        if (forceSync) {
+            forceSync(account, authority);
+        }
     }
 
+    /**
+     * Add s Sync Account
+     *
+     * @param account account
+     */
+    public void addAccount(Account account) {
+        AccountManager accountManager = (AccountManager) getContext().getSystemService(Context.ACCOUNT_SERVICE);
+        accountManager.addAccountExplicitly(account, account.type, null);
+    }
 
+    /**
+     * Checks whether this sync account exists
+     *
+     * @param account the syncAccount
+     * @return isExists
+     */
+    public boolean isAccountExists(Account account) {
+        return isExistsUserAccount(account.type);
+    }
+
+    /**
+     * Checks whether this sync account exists
+     *
+     * @param accountType the type of account
+     * @return isExists
+     */
+    public boolean isExistsUserAccount(String accountType) {
+        return isAccountExists(accountType);
+    }
+
+    /**
+     * Checks whether this sync acccount exists
+     *
+     * @param accountType the account type
+     * @return isExists
+     */
     public boolean isAccountExists(String accountType) {
         AccountManager accountManager = (AccountManager) getContext().getSystemService(Context.ACCOUNT_SERVICE);
         Account[] accounts = accountManager.getAccounts();
@@ -44,12 +106,15 @@ public class UserAccountsManager {
         return false;
     }
 
-    public boolean isExistsUserAccount(String accountType) {
-        return isAccountExists(accountType);
-    }
-
+    /**
+     * @param account
+     * @param authority
+     */
     public void forceSync(Account account, String authority) {
 
+        if (!isAccountExists(account)) {
+            addAccount(account);
+        }
         Bundle extras = new Bundle();
         extras.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);//Force a manual sync
         extras.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);//Make SyncExecutor Start Immediatley
