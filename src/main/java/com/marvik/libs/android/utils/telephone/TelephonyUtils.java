b4c13d9f02db5b84d68017f089923a4d976da34f
Victor Mwenda <vmwenda.vm@gmail.com>
2016-12-27 11:58:58 +0300

    Added methods to use reflection to get telephony utils info, added method to support old devices and back compatibility

--- src/main/java/com/marvik/libs/android/utils/telephone/TelephonyUtils.java
+++ src/main/java/com/marvik/libs/android/utils/telephone/TelephonyUtils.java
@@ -3,6 +3,8 @@ package com.marvik.libs.android.utils.telephone;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
@@ -123,6 +125,7 @@ public class TelephonyUtils {
     public String getLine1Number() {
         return getTelephonyManager().getLine1Number();
     }
+
     public String getLineNumber() {
         try {
             Method method = getTelephonyManager().getClass().getMethod("getLine1Number", Integer.class);
@@ -134,7 +137,8 @@ public class TelephonyUtils {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        return "unknown";}
+        return "unknown";
+    }
 
     public int getSubId() {
         try {
@@ -147,7 +151,8 @@ public class TelephonyUtils {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        return -1;}
+        return -1;
+    }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     public List<SubscriptionInfo> getActiveSubscriptionInfoList() {
@@ -249,5 +254,169 @@ public class TelephonyUtils {
             e.printStackTrace();
         }
         return "unknown";
+    }
+
+    public boolean isNetworkRoaming() {
+        return getTelephonyManager().isNetworkRoaming();
+    }
+
+    public String getMobileNetwork() {
+        return getTelephonyManager().getNetworkOperator();
+    }
+
+    public String getMobileNetworkCode() {
+        return getTelephonyManager().getNetworkOperator();
+    }
+
+    public CharSequence getNetworkOperator() {
+        return getTelephonyManager().getNetworkOperator();
+    }
+
+    public String getNetworkOperatorName() {
+        return getTelephonyManager().getNetworkOperatorName();
+    }
+
+    public String getNetworkType() {
+        return lookUpNetworkType(getTelephonyManager().getNetworkType());
+    }
+
+    @NonNull
+    private String lookUpNetworkType(int networkType) {
+        switch (networkType) {
+            /** Network type is unknown */
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                return "Unknown";
+            /** Current network is GPRS */
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return "GPRS";
+            /** Current network is EDGE */
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return "EDGE";
+            /** Current network is UMTS */
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return "UMTS";
+            /** Current network is CDMA: Either IS95A or IS95B*/
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+                return "CDMA";
+            /** Current network is EVDO revision 0*/
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                return "EVDO r0";
+            /** Current network is EVDO revision A*/
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                return "EVDO rA";
+            /** Current network is 1xRTT*/
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+                return "1xRTT";
+            /** Current network is HSDPA */
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return "HSDPA";
+            /** Current network is HSUPA */
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return "HSUPA";
+            /** Current network is HSPA */
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return "HSPA";
+            /** Current network is iDen */
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return "iDen";
+            /** Current network is EVDO revision B*/
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                return "EVDO rB";
+            /** Current network is LTE */
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return "LTE";
+            /** Current network is eHRPD */
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+                return "eHRPD";
+            /** Current network is HSPA+ */
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                return "HSPA+";
+            /** Current network is GSM   */
+            case 16:
+                return "GSM";
+            /** Current network is TD_SCDMA   */
+            case 17:
+                return "TD_SCDMA";
+            /** Current network is IWLAN  */
+            case 18:
+                return "IWLAN";
+        }
+        return "Unknown";
+    }
+
+    public String getMobileCountryCode() {
+        return getTelephonyManager().getNetworkCountryIso();
+    }
+
+    public String getMMSUserAgentProfURL() {
+        return getTelephonyManager().getMmsUAProfUrl();
+    }
+
+    public String getMMSUserAgent() {
+        return getTelephonyManager().getMmsUserAgent();
+    }
+
+    public String getVoiceMailAlphaTag() {
+        return getTelephonyManager().getVoiceMailAlphaTag();
+    }
+
+    public String getVoiceMailNumber() {
+        return getTelephonyManager().getVoiceMailNumber();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String getVoiceMailNetworkType() {
+        return lookUpNetworkType(getTelephonyManager().getVoiceNetworkType());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public boolean isSMSCapable() {
+        return getTelephonyManager().isSmsCapable();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+    public boolean isVoiceCapable() {
+        return getTelephonyManager().isVoiceCapable();
+    }
+
+    public String getDeviceSoftwareVersion() {
+        return getTelephonyManager().getDeviceSoftwareVersion();
+    }
+
+    public String getGroupIdLevel() {
+        return getTelephonyManager().getGroupIdLevel1();
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    public boolean hasHearingAidCompatibility() {
+        return getTelephonyManager().isHearingAidCompatibilitySupported();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean hasTTYMode() {
+        return getTelephonyManager().isTtyModeSupported();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+    public boolean hasCarriePrivileges() {
+        return getTelephonyManager().hasCarrierPrivileges();
+    }
+
+    public String getCellLocation() {
+        return getTelephonyManager().getCellLocation().toString();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean canChangeDTMFToneLength() {
+        return getTelephonyManager().canChangeDtmfToneLength();
+    }
+
+    public boolean hasICCCard() {
+        return getTelephonyManager().hasIccCard();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean isWorldPhone() {
+        return getTelephonyManager().isWorldPhone();
     }
 }
