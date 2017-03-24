e2bee7ce2f797c3b8859e719076ca365825389ed
Victor Mwenda <vmwenda.vm@gmail.com>
2017-03-24 18:32:09 +0300

    Fix bug on missing file separator, uri parser

--- src/main/java/com/marvik/libs/android/downloader/Downloader.java
+++ src/main/java/com/marvik/libs/android/downloader/Downloader.java
@@ -7,63 +7,44 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-/**
- * Downloader
- * Provides an API to download files and save the downloaded file to the file system
- */
+
 public final class Downloader {
 
     private static Context context;
 
-    private Downloader() {
-    }
+    private Downloader() {}
 
-    /**
-     * Returns an instance of this class allowing singleton access
-     *
-     * @param context
-     * @return
-     */
     public static Downloader getInstance(Context context) {
         Downloader.context = context;
         return new Downloader();
     }
 
-    /**
-     * Download file and save on the file path
-     *
-     * @param fileUri       the url of the file
-     * @param filePath      the storage path of the file
-     * @param successIntent success intent to send when file downloaded and saved
-     * @param errorIntent   error intent to send when file download fails
-     */
-    public void downloadFile(final String fileUri, final String filePath, Intent successIntent,
-                             Intent errorIntent) {
-
+    public void downloadFile(final String fileUri,final String filePath) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    URL url = new URL(fileUri);
+                    URL url = new URL(parseUrl(fileUri));
                     URLConnection urlConnection = url.openConnection();
                     InputStream inputStream = urlConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
-                    String filename = new File(fileUri).getName();
-
+                    String filename = fileUri.substring(fileUri.lastIndexOf("/"));
                     int count = 0;
                     byte[] buffer = new byte[1024];
 
-                    File fileDir = new File(filePath).getParentFile();
+                    File fileDir = new File(filePath);
 
                     if (!fileDir.exists()) {
                         fileDir.mkdirs();
                     }
 
-                    File downloadFile = new File(fileDir + filename);
+                    File downloadFile = new File(fileDir + File.separator + filename);
 
                     //Ensure that we do not always download existing files
                     if (downloadFile.exists()) {
@@ -71,30 +52,36 @@ public final class Downloader {
                     }
 
                     FileOutputStream fileOutputStream = new FileOutputStream(downloadFile);
-
                     while ((count = inputStream.read(buffer)) != -1) {
                         fileOutputStream.write(buffer, 0, count);
                     }
 
-                    context.sendBroadcast(successIntent);
+                    context.sendBroadcast(new Intent("com.marvik.libs.android.downloader.Downloader.ACTION_FILE_DOWNLOADED"));
 
                 } catch (MalformedURLException e) {
-
                     e.printStackTrace();
-
-                    errorIntent.putExtra("error", e.getMessage());
-                    context.sendBroadcast(errorIntent);
-
                 } catch (IOException e) {
-
                     e.printStackTrace();
-
-                    errorIntent.putExtra("error", e.getMessage());
-                    context.sendBroadcast(errorIntent);
-
                 }
             }
         }).start();
 
+    }
+    
+     /**
+     * Parse fileUri
+     * Replaces bad format characters like an empty space from a file uri
+     *
+     * @param fileUri
+     * @return
+     */
+    private String parseUrl(String fileUri) {
+
+        fileUri = fileUri.replace(" ", "%20");
+
+        if (fileUri.contains(" ")) {
+            return parseUrl(fileUri);
+        }
+        return fileUri;
     }
 }
