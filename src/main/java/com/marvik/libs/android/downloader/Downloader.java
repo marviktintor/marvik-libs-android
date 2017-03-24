e1934b5bc74c2e46e1bd44b824f02a2e2fa10e07
Victor Mwenda <vmwenda.vm@gmail.com>
2017-03-24 16:47:03 +0300

    Added success and error intents, better filenames

--- src/main/java/com/marvik/libs/android/downloader/Downloader.java
+++ src/main/java/com/marvik/libs/android/downloader/Downloader.java
@@ -7,24 +7,43 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-
+/**
+ * Downloader
+ * Provides an API to download files and save the downloaded file to the file system
+ */
 public final class Downloader {
 
     private static Context context;
 
-    private Downloader() {}
+    private Downloader() {
+    }
 
+    /**
+     * Returns an instance of this class allowing singleton access
+     *
+     * @param context
+     * @return
+     */
     public static Downloader getInstance(Context context) {
         Downloader.context = context;
         return new Downloader();
     }
 
-    public void downloadFile(final String fileUri,final String filePath) {
+    /**
+     * Download file and save on the file path
+     *
+     * @param fileUri       the url of the file
+     * @param filePath      the storage path of the file
+     * @param successIntent success intent to send when file downloaded and saved
+     * @param errorIntent   error intent to send when file download fails
+     */
+    public void downloadFile(final String fileUri, final String filePath, Intent successIntent,
+                             Intent errorIntent) {
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -32,13 +51,13 @@ public final class Downloader {
                     URL url = new URL(fileUri);
                     URLConnection urlConnection = url.openConnection();
                     InputStream inputStream = urlConnection.getInputStream();
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
-                    String filename = fileUri.substring(fileUri.lastIndexOf("/"));
+                    String filename = new File(fileUri).getName();
+
                     int count = 0;
                     byte[] buffer = new byte[1024];
 
-                    File fileDir = new File(filePath);
+                    File fileDir = new File(filePath).getParentFile();
 
                     if (!fileDir.exists()) {
                         fileDir.mkdirs();
@@ -52,16 +71,27 @@ public final class Downloader {
                     }
 
                     FileOutputStream fileOutputStream = new FileOutputStream(downloadFile);
+
                     while ((count = inputStream.read(buffer)) != -1) {
                         fileOutputStream.write(buffer, 0, count);
                     }
 
-                    context.sendBroadcast(new Intent("com.marvik.libs.android.downloader.Downloader.ACTION_FILE_DOWNLOADED"));
+                    context.sendBroadcast(successIntent);
 
                 } catch (MalformedURLException e) {
+
                     e.printStackTrace();
+
+                    errorIntent.putExtra("error", e.getMessage());
+                    context.sendBroadcast(errorIntent);
+
                 } catch (IOException e) {
+
                     e.printStackTrace();
+
+                    errorIntent.putExtra("error", e.getMessage());
+                    context.sendBroadcast(errorIntent);
+
                 }
             }
         }).start();
