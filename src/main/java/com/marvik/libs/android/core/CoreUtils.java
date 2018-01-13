1f524ee43c703c84769f82d3ea7fee67babcd8f0
Victor Mwenda <vmwenda.vm@gmail.com>
2018-01-13 20:58:54 +0300

    Upgraded to android api 27 and libtools 27.0.1

--- src/main/java/com/marvik/libs/android/core/CoreUtils.java
+++ src/main/java/com/marvik/libs/android/core/CoreUtils.java
@@ -1,58 +1,14 @@
 package com.marvik.libs.android.core;
 
-import android.app.AlarmManager;
-import android.app.AlertDialog;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.support.annotation.IntegerRes;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.NotificationCompat;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.TextView;
-
-import com.marvik.libs.android.core.date.CalendarUtils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
-import java.util.regex.Pattern;
-
-import static java.security.AccessController.getContext;
 
 
 public class CoreUtils {
