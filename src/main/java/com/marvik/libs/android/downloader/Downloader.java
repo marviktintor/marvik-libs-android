191ebb87402d23b30a898222be3825b22f2f4028
Victor Mwenda <vmwenda.vm@gmail.com>
2018-01-17 15:40:04 +0300

    Added filepath extra to intent, fix bugs on API

--- src/main/java/com/marvik/libs/android/downloader/Downloader.java
+++ src/main/java/com/marvik/libs/android/downloader/Downloader.java
@@ -15,6 +15,8 @@ import java.net.URLConnection;
 
 public final class Downloader {
 
+    public static final String EXTRA_FILEPATH = "filepath";
+
     /**
      * Downloads a file
      *
@@ -25,54 +27,47 @@ public final class Downloader {
      * @param overWrite
      * @param downloadIntent
      */
-    public void downloadFile(final Context context, final String downloadURI, final String storeDir,
-                             @Nullable String fileName, final boolean overWrite, final Intent downloadIntent) {
+    public static void downloadFile(final Context context, final String downloadURI, final String storeDir,
+                                    @Nullable String fileName, final boolean overWrite, final Intent downloadIntent) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL(parseUrl(downloadURI));
-                    URLConnection urlConnection = url.openConnection();
-                    InputStream inputStream = urlConnection.getInputStream();
+        new Thread(() -> {
+            try {
+                URL url = new URL(parseUrl(downloadURI));
+                URLConnection urlConnection = url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
 
-                    File remoteFile = new File(downloadURI);
-
-                    String filename = remoteFile.getName();
-
-                    File downloadFile = new File(storeDir + File.pathSeparator + filename);
+                File downloadFile = new File(storeDir + File.separator + fileName);
 
 
-                    int count = 0;
+                int count = 0;
 
-                    byte[] buffer = new byte[1024];
+                byte[] buffer = new byte[1024];
 
-                    File fileDir = new File(storeDir);
+                File fileDir = new File(storeDir);
 
-                    if (!fileDir.exists()) {
-                        fileDir.mkdirs();
-                    }
-
-                    if (!overWrite) {
-                        //Ensure that we do not always download existing files
-                        if (downloadFile.exists()) {
-                            return;
-                        }
-                    }
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(downloadFile);
-
-                    while ((count = inputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, count);
-                    }
-
-                    context.sendBroadcast(downloadIntent);
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (!fileDir.exists()) {
+                    fileDir.mkdirs();
                 }
+
+                if (!overWrite) {
+                    //Ensure that we do not always download existing files
+                    if (downloadFile.exists()) {
+                        return;
+                    }
+                }
+
+                FileOutputStream fileOutputStream = new FileOutputStream(downloadFile);
+
+                while ((count = inputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, count);
+                }
+                downloadIntent.putExtra(Downloader.EXTRA_FILEPATH, downloadFile.getAbsolutePath());
+                context.sendBroadcast(downloadIntent);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }).start();
 
