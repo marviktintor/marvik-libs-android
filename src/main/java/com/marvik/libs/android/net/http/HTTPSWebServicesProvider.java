6c491057f3d0255016c917d9c65402b21b248948
Victor Mwenda <vmwenda.vm@gmail.com>
2020-02-23 19:39:24 +0300

    Added code to upload files to the server

--- src/main/java/com/marvik/libs/android/net/http/HTTPSWebServicesProvider.java
+++ src/main/java/com/marvik/libs/android/net/http/HTTPSWebServicesProvider.java
@@ -1,14 +1,22 @@
 package com.marvik.libs.android.net.http;
 
 
+import android.util.Log;
+import android.widget.Toast;
+
 import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.UnknownServiceException;
 import java.security.NoSuchAlgorithmException;
@@ -353,6 +361,82 @@ public abstract class HTTPSWebServicesProvider<K, V> {
         }
 
 
+    }
+
+
+    protected boolean uploadFile(File file, URL url, HttpsURLConnection conn) throws IOException {
+
+
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+            // open a URL connection to the Servlet
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+
+            // Open a HTTP  connection to  the URL
+            conn = (HttpsURLConnection) url.openConnection();
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            conn.setRequestProperty("uploaded_file", file.getName());
+
+          DataOutputStream  dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name="+file.getName()+";filename="
+                            + file.getName() + "" + lineEnd);
+
+                    dos.writeBytes(lineEnd);
+
+            // create a buffer of  maximum size
+            bytesAvailable = fileInputStream.available();
+
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            }
+
+            // send multipart form data necesssary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+           int serverResponseCode = conn.getResponseCode();
+            String serverResponseMessage = conn.getResponseMessage();
+
+            Log.i("uploadFile", "HTTP Response is : "
+                    + serverResponseMessage + ": " + serverResponseCode);
+
+            if(serverResponseCode == 200){
+
+
+            }
+
+            //close the streams //
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
+
+            return true;
     }
 
     /**
